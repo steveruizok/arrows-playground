70da6fe94c9caf1d7fdaa6ef01d01a93d9727fcb
Steve <steveruizok@gmail.com>
2020-10-17 21:42:01 +0100

    last commit before replacing

--- src/components/brain/ui.tsx
+++ src/components/brain/ui.tsx
@@ -1,5 +1,5 @@
-import { createState } from "@state-designer/react"
-import { IBox, IArrow } from "../types"
+import { createState, createDesign } from "@state-designer/react"
+import { IBox, IFrame, IArrow, IBoxSnapshot } from "../types"
 import store from "./store"
 
 let scale = 1
@@ -10,18 +10,295 @@ const scroll = { x: 2500, y: 2500 }
 const scrollOrigin = { x: 2500, y: 2500 }
 const viewport = { width: 0, height: 0 }
 
-export default createState({
-	data: {},
+const design = createDesign({
+	data: {
+		spawning: {
+			boxes: {} as Record<string, IBox>,
+			arrows: {} as Record<string, IArrow>,
+			clones: {} as Record<string, IBox>,
+		},
+		selecting: {
+			brush: {} as IFrame,
+			boxes: {} as Record<string, IBox>,
+			arrows: {} as Record<string, IArrow>,
+		},
+		initial: {
+			boxes: {} as Record<string, IBoxSnapshot>,
+		},
+	},
 	on: {},
-	actions: {},
+	initial: "selecting",
+	states: {
+		selecting: {
+			initial: "selectingIdle",
+			states: {
+				selectingIdle: {
+					on: {
+						CANCELLED: { do: "clearSelection" },
+						STARTED_CLICKING_BOX: { to: "clickingBox" },
+						STARTED_CLICKING_CANVAS: { to: "brushSelecting" },
+					},
+				},
+				clickingBox: {
+					onEnter: "setInitialSnapshot",
+					on: {
+						DRAGGED_BOX: { if: "dragIsFarEnough", to: "draggingBox" },
+					},
+				},
+				clickingArrowNode: {
+					on: {
+						DRAGGED_ARROW_NODE: { if: "dragIsFarEnough", to: "drawingArrow" },
+						RELEASED_ARROW_NODE: { to: "pickingArrow" },
+					},
+				},
+				brushSelecting: {
+					on: {
+						MOVED_POINTER: { do: "updateSelectionBrush" },
+						SCROLLED: { do: "updateSelectionBrush" },
+						RAISED_POINTER: { do: "completeSelection" },
+					},
+					initial: "settingSelection",
+					states: {
+						settingSelection: {
+							on: {
+								MOVED_POINTER: {
+									get: "brushSelectedBoxes",
+									do: "setBrushSelectedToSelection",
+								},
+								SCROLLED: {
+									get: "brushSelectedBoxes",
+									do: "setBrushSelectedToSelection",
+								},
+							},
+						},
+						pushingToSelection: {
+							on: {
+								MOVED_POINTER: {
+									get: "brushSelectedBoxes",
+									do: "pushBrushSelectedToSelection",
+								},
+								SCROLLED: {
+									get: "brushSelectedBoxes",
+									do: "pushBrushSelectedToSelection",
+								},
+							},
+						},
+					},
+				},
+				draggingBoxes: {
+					states: {
+						dragOperation: {
+							initial: "notCloning",
+							states: {
+								notCloning: {
+									onEnter: "clearDraggingBoxesClones",
+									on: {
+										ENTERED_OPTION_MODE: { to: "cloning" },
+										RAISED_POINTER: { do: "completeSelectedBoxes" },
+										CANCELLED: {
+											do: "restoreInitialBoxes",
+											to: "selectingIdle",
+										},
+									},
+								},
+								cloning: {
+									onEnter: "createDraggingBoxesClones",
+									on: {
+										ENTERED_OPTION_MODE: { to: "notCloning" },
+										RAISED_POINTER: {
+											do: ["completeSelectedBoxes", "completeBoxesFromClones"],
+										},
+										CANCELLED: {
+											do: ["restoreInitialBoxes", "clearDraggingBoxesClones"],
+											to: "selectingIdle",
+										},
+									},
+								},
+							},
+						},
+						axes: {
+							initial: "freeAxes",
+							states: {
+								freeAxes: {
+									onEnter: "updateDraggingBoxesToLockedAxes",
+									on: {
+										ENTERED_SHIFT_MODE: { to: "lockedAxes" },
+									},
+								},
+								lockedAxes: {
+									onEnter: "updateDraggingBoxesToFreeAxes",
+									on: {
+										EXITED_SHIFT_MODE: { to: "freeAxes" },
+									},
+								},
+							},
+						},
+					},
+				},
+				resizingBoxes: {
+					on: {
+						CANCELLED: { do: "restoreInitialBoxes", to: "selectingIdle" },
+						RAISED_POINTER: { do: "completeSelectedBoxes" },
+					},
+					initial: "edgeResizing",
+					states: {
+						edgeResizing: {
+							on: {
+								MOVED_POINTER: { do: "cornerResizeSelectedBoxes" },
+								SCROLLED: { do: "cornerResizeSelectedBoxes" },
+							},
+						},
+						cornerResizing: {
+							on: {
+								MOVED_POINTER: { do: "edgeResizeSelectedBoxes" },
+								SCROLLED: { do: "edgeResizeSelectedBoxes" },
+							},
+							initial: "freeRatio",
+							states: {
+								freeRatio: {
+									onEnter: "updateResizingBoxesToLockedRatio",
+									on: {
+										ENTERED_SHIFT_MODE: { to: "lockedRatio" },
+									},
+								},
+								lockedRatio: {
+									onEnter: "updateResizingBoxesToFreeRatio",
+									on: {
+										EXITED_SHIFT_MODE: { to: "freeRatio" },
+									},
+								},
+							},
+						},
+					},
+				},
+				creatingArrow: {
+					initial: "drawingArrow",
+					on: 
+					states: {
+						drawingArrow: {},
+						pickingArrow: {},
+					},
+				},
+			},
+		},
+		drawingBox: {
+			on: {
+				CANCELLED: { to: "selecting" },
+			},
+			initial: "notDrawing",
+			states: {
+				notDrawing: {},
+			},
+		},
+		pickingArrow: {
+			initial: "choosingFrom",
+			on: {
+				CANCELLED: { to: "selecting" },
+			},
+			states: {
+				choosingFrom: {},
+				choosingTo: {},
+			},
+		},
+	},
+	results: {
+		brushSelectedBoxes() {},
+	},
+	conditions: {
+		dragIsFarEnough() {
+			return true
+		},
+	},
+	actions: {
+		// Selection
+		clearSelection() {},
+		completeSelection() {},
+		updateSelectionBrush() {},
+		setBrushSelectedToSelection() {},
+		pushBrushSelectedToSelection() {},
+		setInitialSnapshot() {},
+		// Drawing Arrow
+		createDrawingArrow() {},
+		setDrawingArrowTarget() {},
+		completeDrawingArrow() {},
+		clearDrawingArrow() {},
+		// Arrows
+		updateSelectedArrows() {},
+		flipSelectedArrows() {},
+		invertSelectedArrows() {},
+		// Arrows to Boxes
+		updateArrowsToSelectedBoxes() {},
+		flipArrowsToSelectedBoxes() {},
+		invertArrowsToSelectedBoxes() {},
+		// Drawing Box
+		createDrawingBox() {},
+		updateDrawingBox() {},
+		completeDrawingBox() {},
+		clearDrawingBox() {},
+		// Boxes
+		dragSelectedBoxes() {},
+		edgeResizeSelectedBoxes() {},
+		cornerResizeSelectedBoxes() {},
+		updateResizingBoxesToFreeRatio() {},
+		updateResizingBoxesToLockedRatio() {},
+		updateDraggingBoxesToFreeAxes() {},
+		updateDraggingBoxesToLockedAxes() {},
+		restoreInitialBoxes() {},
+		completeSelectedBoxes() {},
+		// Clones
+		clearDraggingBoxesClones() {},
+		createDraggingBoxesClones() {},
+		completeBoxesFromClones() {},
+	},
+	values: {},
 })
+
+const state = createState(design)
+
+export default state
+
+// Key Presses (not part of state chart, but still may cause events)
+
+const keyDownActions = {
+	Escape: "CANCELLED",
+	Alt: "ENTERED_ALT_MODE",
+	" ": "ENTERED_SPACE_MODE",
+	Shift: "ENTERED_SHIFT_MODE",
+	Control: "ENTERED_CONTROL_MODE",
+	Meta: "ENTERED_META_MODE",
+}
+
+const keyUpActions = {
+	Alt: "EXITED_ALT_MODE",
+	" ": "EXITED_SPACE_MODE",
+	Shift: "EXITED_SHIFT_MODE",
+	Control: "EXITED_CONTROL_MODE",
+	Meta: "EXITED_META_MODE",
+	f: "SELECTED_DRAWING",
+	v: "SELECTED_SELECTING",
+}
 
 export function handleKeyDown(e: KeyboardEvent) {
 	pressedKeys[e.key] = true
+	const action = keyDownActions[e.key]
+	if (action) {
+		state.send(action)
+	}
+	// Handle shift here?
 }
 
 export function handleKeyUp(e: KeyboardEvent) {
 	pressedKeys[e.key] = false
+	const action = keyUpActions[e.key]
+	if (action) {
+		state.send(action)
+	}
+}
+
+export function handleKeyPress(e: KeyboardEvent) {
+	if (e.key === " " && !state.isInAny("editingLabel", "editingArrowLabel")) {
+		e.preventDefault()
+	}
 }
 
 export function handlePointerMove(e: PointerEvent) {
@@ -39,9 +316,18 @@ export function handlePointerDown(e: PointerEvent) {
 export function handleScroll(left: number, top: number) {
 	scroll.x = left
 	scroll.y = top
+	state.send("SCROLLED")
+	state.send("MOVED_POINTER") // TODO: Combine in the state machine.
 }
 
 export function handleViewportChange(width: number, height: number) {
 	viewport.width = width
 	viewport.height = height
+	state.send("CHANGED_VIEWPORT")
+}
+
+export function handleScaleChange(value: number) {
+	scale = value
+	state.send("SCALED")
+	state.send("MOVED_POINTER") // TODO: Combine in the state machine.
 }
--- src/components/types.tsx
+++ src/components/types.tsx
@@ -1,39 +1,49 @@
 export interface IPoint {
-	x: number
-	y: number
+  x: number
+  y: number
 }
 
 export interface ISize {
-	width: number
-	height: number
+  width: number
+  height: number
 }
 
 export interface IFrame extends IPoint, ISize {}
 
 export interface IBounds extends IPoint, ISize {
-	maxX: number
-	maxY: number
+  maxX: number
+  maxY: number
 }
 
 export interface IBox extends IFrame {
-	id: string
-	label: string
-	color: string
-	start: IFrame & {
-		nx: number
-		ny: number
-		nmx: number
-		nmy: number
-		nw: number
-		nh: number
-	}
+  id: string
+  label: string
+  color: string
+  start: IFrame & {
+    nx: number
+    ny: number
+    nmx: number
+    nmy: number
+    nw: number
+    nh: number
+  }
+}
+
+export interface IBoxSnapshot extends IFrame {
+  id: string
+  nx: number
+  ny: number
+  nmx: number
+  nmy: number
+  nw: number
+  nh: number
 }
 
 export interface IArrow {
-	id: string
-	from: string
-	to: string
-	flip: boolean
-	label: string
-	points: number[]
+  id: string
+  from: string
+  to: string
+  flip: boolean
+  label: string
+  points: number[]
 }
